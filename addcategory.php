15325eff4d029a48471714b40b909eb61e371808
Rashedul Islam <rashedulislam.ruet@gmail.com>
2019-10-15 12:06:45 +0600

    change add catagory connection

--- addcategory.php
+++ addcategory.php
@@ -8,7 +8,7 @@ if (!empty($_POST['cat_value'])) {
 
     $sql = "INSERT INTO tbl_cat (cat_name,cat_value) VALUES (:cat_name,:cat_value)";
 
-    $stmt = $conn->prepare($sql);
+    $stmt = $connection->prepare($sql);
 
     $stmt->bindParam(':cat_name', $cat);
     $stmt->bindParam(':cat_value', $_POST['cat_value']);
