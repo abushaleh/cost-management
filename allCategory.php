9cc8364a80dc991145a4848f19973bf080ce178a
Rashedul Islam <rashedulislam.ruet@gmail.com>
2019-10-16 11:38:57 +0600

    added catagory again

--- allCategory.php
+++ allCategory.php
@@ -1,43 +1,30 @@
 <?php
 require 'db.php';
 
-if (isset($_GET['multiple'])) {
-    $multiple = $_GET['multiple'];
-    $query = "SELECT * FROM tbl_cost WHERE YEAR(cost_date) = '2019' AND MONTH(cost_date) = '12' ";
-    $stmt = $connection->prepare($query);
-    $stmt->execute();
+$query = "SELECT cat_name,SUM(cost_amount) as column_sum FROM tbl_cat INNER JOIN tbl_cost GROUP BY cat_name ";
+$stmt = $connection->prepare($query);
+$stmt->execute();
+$data = $stmt->fetchAll();
 
-}
 ?>
 
 <?php include 'header.php';?>
 
-<div>
-    <form action="" method="get">
-        <input type="date" name="multiple" placeholder="Search keyword..." />
-        <input type="submit" name="submit" value="Search" />
-    </form>
-</div>
+
 <div>
     <table border="1">
         <thead>
             <tr>
-                <th>cost_name</th>
-                <th>cost_details</th>
-                <th>cost_amount</th>
-                <th>cost_date</th>
                 <th>cat_name</th>
+                <th>Amount</th>
             </tr>
         </thead>
         <?php
-foreach ($stmt as $value) {
+foreach ($data as $value) {
     ?>
         <tr>
-            <td><?php echo $value['cost_name']; ?></td>
-            <td><?php echo $value['cost_details']; ?></td>
-            <td><?php echo $value['cost_amount']; ?></td>
-            <td><?php echo $value['cost_date']; ?></td>
             <td><?php echo $value['cat_name']; ?></td>
+            <td><?php echo $value['column_sum']; ?></td>
         </tr>
         <?php }?>
     </table>
