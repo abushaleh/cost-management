28c9b7e563a4c82c452a41028f4be13a21b2ec19
Rashedul Islam <rashedulislam.ruet@gmail.com>
2019-10-16 10:34:23 +0600

    changes link of catagory

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ $costs = $state->fetchAll();
       <td><?php echo $cost['cost_details']; ?></td>
       <td><?php echo $cost['cost_amount']; ?></td>
       <td>
-        <a href="category.php?id=<?php $cost['cat_id'];?>">
+        <a href="single_category.php?id=<?php $cost['cat_id'];?>">
           <?php echo $cost['cat_name']; ?>
         </a>
       </td>
